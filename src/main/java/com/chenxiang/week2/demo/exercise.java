020baaf4a08cfa2f1f2d9327090bec1ca4c3da1f
2019211001001010-ChenXiang <269472640@qq.com>
2021-03-17 10:54:15 +0800

    HW-Week 2 commit

---
+++ src/main/java/com/chenxiang/week2/demo/exercise.java
@@ -0,0 +1,27 @@
+package com.chenxiang.week2.demo;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//now its just a java class
+//extend HttpServlet
+public class exercise extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println( "Name: ChenXiang");
+        writer.println( "ID: 2019211001001010");
+        writer.println( "Date and Time: March 15 18:00 Rainy 2021");// that all
+        //next we need to tell about this servlet to tomcat - how ? - web.xml
+
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response){
+        //when client request method is Post - here - inside doPost()
+
+    }
+}
+
